330deba2789770b821dc0d2d1877cf5903037369
AudeFi <odefi@hotmail.fr>
2015-12-15 20:34:49 +0100

    jquery librairie scroll
    Ajout de la librairie jquery pour detecter le scroll

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
 var input_research = document.querySelector('.input-research');
 var force_nav = document.querySelector('.force-nav');
 var force_link = document.querySelector('.force-a');
+var video = document.querySelector('.video-section');
+console.log(video.height);
 
 input_research.addEventListener('click',function(){
 	var not_available = document.querySelector(".noresearch");
@@ -16,4 +18,16 @@ force_link.addEventListener('mouseover', function(){
 
 force_nav.addEventListener('mouseleave', function(){
 	force_nav.classList.remove("visible");
+});
+
+
+$(document).ready(function(){
+	$(window).scroll(function() {
+
+    //if I scroll more than 1000px...
+    if($(window).scrollTop() > video.height){
+         //do whatever
+         console.log('scroll');
+    }
+});
 });
